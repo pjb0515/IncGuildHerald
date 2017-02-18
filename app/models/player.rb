7de28690d29d848573e7010a14c21edc840b21eb
Peter Benoit <pjb0515@outlook.com>
2017-02-18 17:45:52 -0500

    fixing bug in player.rb

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,6 +8,7 @@ class Player < ActiveRecord::Base
 	enum realm: [ :hibernia, :midgard, :albion, "7" ]
   
   self.create_player(name, guild_name, race, daoc_class, realm, level, realm_level, total_rps, last_update)
+    new_player = nil
     if !Player.exists?(:name => name)
       player_guild = nil
       if Guild.exists?(:name => guild_name)
