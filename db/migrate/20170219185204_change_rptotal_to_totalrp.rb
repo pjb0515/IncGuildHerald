64724ad534d5f7d321acdce07cfeca6f2a94ca0f
Peter Benoit <pjb0515@outlook.com>
2017-02-19 14:49:26 -0500

    misnamed total_rps in migration

--- db/migrate/20170219185204_change_rptotal_to_totalrp.rb
+++ db/migrate/20170219185204_change_rptotal_to_totalrp.rb
@@ -1,6 +1,6 @@
 class ChangeRptotalToTotalrp < ActiveRecord::Migration
   def change
     remove_column :rp_snapshots, :rp_total, :integer
-    add_column :rp_snapshots, :total_rp, :integer
+    add_column :rp_snapshots, :total_rps, :integer
   end
 end
