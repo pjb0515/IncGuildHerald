c8b82c0ad0268f8a15db43794b1ea7225e98cbaf
Peter Benoit <pjb0515@outlook.com>
2017-02-21 20:10:22 -0500

    optimization tests

--- app/controllers/herald_controller.rb
+++ app/controllers/herald_controller.rb
@@ -6,6 +6,8 @@ class HeraldController < ApplicationController
   end
   
   def get_dump
+    parser = Yajl::Parser.new
+    
     Spawnling.new do
       url = URI.parse('https://uthgard.org/herald/api/dump')
       
@@ -19,10 +21,23 @@ class HeraldController < ApplicationController
       single_player_splits = res.body.split(/"[a-z]+": {/)
       
       single_player_splits.each do |single_player_json|
-        single_player_json = single_player_json.tr("},", "}")
+        single_player_json = single_player_json.gsub(/},/, "}")
         single_player_json = "{ " + single_player_json
         
-        puts single_player_json
+        player_hash = parser.parse(single_player_json)
+        
+        name = player_hash["Name"]
+        guild_name = player_hash["Guild"]
+        race = player_hash["Race"].downcase
+        daoc_class = player_hash["Class"].downcase
+        realm = player_hash["Realm"].downcase
+        last_update = player_hash["LastUpdated"]
+        level = player_hash["Level"]
+        realm_level = player_hash["RealmRank"]
+        total_rps = player_hash["Rp"]
+        
+        Player.update_player(name, guild_name, race, daoc_class, realm, level, realm_level, total_rps, last_update)
+      
       end
     end
   end
