8fe13a7e0b747db6b6bed6ebaeea3e0ccc91093b
Peter Benoit <pjb0515@outlook.com>
2017-02-22 11:30:47 -0500

    testing a morris.js fix

--- app/assets/javascripts/SiteWide/morris-data.js
+++ app/assets/javascripts/SiteWide/morris-data.js
@@ -2,7 +2,7 @@
 
 $(function() {
 
-  if ( $( ".morris-donut-chart" ).length && $( ".morris-donut-chart" ).children().length == 0) {
+  if ( $( "#morris-donut-chart" ).length && $( "#morris-donut-chart" ).children().length == 0) {
     // Donut Chart
     Morris.Donut({
         element: 'morris-donut-chart',
