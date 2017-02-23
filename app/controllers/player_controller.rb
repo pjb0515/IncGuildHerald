b5a184c4f73ffd81395e34d5b655ab84d8123c11
Peter Benoit <pjb0515@outlook.com>
2017-02-23 14:20:54 -0500

    top players page v1.0

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -3,4 +3,8 @@ class PlayerController < ApplicationController
   def player_details
     @player = Player.find_by(:name => params[:name])
   end
+  
+  def top_players
+    @players = Player.order("total_rps").limit(5)
+  end
 end
