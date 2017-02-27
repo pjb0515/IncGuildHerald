a452600e882dea69a4fb27b0faf1a822772d1675
Peter Benoit <pjb0515@outlook.com>
2017-02-27 00:14:51 -0500

    securing rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,12 @@
 RailsAdmin.config do |config|
 
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Login required') do |username, password|
+      username == ENV['ADMIN_USERNAME'] &&
+      password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
   config.actions do
     dashboard                     # mandatory
     index                         # mandatory
