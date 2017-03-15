422dbd9d9b6d17c8828e95949339da4aaf8cce08
Peter Benoit <pjb0515@outlook.com>
2017-03-14 23:44:45 -0400

    video for inc index

--- app/controllers/inc_controller.rb
+++ app/controllers/inc_controller.rb
@@ -1,6 +1,6 @@
 class IncController < ApplicationController
 
   def index
-    @inc_members = Player.where(guild: Guild.find_by(name: "INC"), level: 49..50).order(:total_rps DESC)
+    @inc_members = Player.where(guild: Guild.find_by(name: "INC"), level: 49..50).order("total_rps DESC")
   end
 end
