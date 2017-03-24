e8c3c9bd91981c01b101247ca74d01ca1c471acf
Peter Benoit <pjb0515@outlook.com>
2017-03-24 17:20:59 -0400

    fixing memory issues

--- app/models/player.rb
+++ app/models/player.rb
@@ -165,12 +165,7 @@ class Player < ActiveRecord::Base
     end
     
     player_list = nil
-    
-    player_list = Rails.cache.fetch("get_rank_player_list/allrealms/overall", expires_in: 20.minutes) do
-      Player.select(:id, :total_rps, :last_three_days_rps, :last_seven_days_rps, last_fourteen_days_rps, :level, :realm).where(level: 45..50).order('total_rps DESC')
-    end
-    player_list = player_list.select {|x| x.realm.eql? realm }
-    
+
     if duration.eql? "overall"
       player_list = Rails.cache.fetch("get_rank_player_list/"+realm+"/overall", expires_in: 20.minutes) do
       
